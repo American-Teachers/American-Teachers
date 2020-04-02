f6bba5281a2c0e4dad84c3660eb8a4f03ba37d8a
Marina Gulakova <marina.gulakova@nike.com>
2020-04-02 11:28:42 -0700

    Fixed merge conflicts

---
+++ wwwroot/src/components/SignUp.js
@@ -0,0 +1,190 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import { Link } from 'react-router-dom';
+import { Grid, Radio, RadioGroup, Box, Typography, Container, Avatar, Button, Checkbox, FormControlLabel }  from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" to={"https://material-ui.com/"}>
+        American Teachers
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  school: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [selectedDate, setSelectedDate] = React.useState(new Date('1990-08-18T21:11:54'));
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.school}>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+            <Typography component="h1" variant="h5">
+                    American Teachers
+                </Typography>
+                <Typography component="h1" variant="h5">
+                    An Online School
+                </Typography>
+                <Typography component="h1" variant="h5">
+                    Education for Everyone
+                </Typography>
+            </Grid>
+        </Grid>
+      </div>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  disableToolbar
+                  variant="inline"
+                  format="MM/dd/yyyy"
+                  margin="normal"
+                  id="date-picker-inline"
+                  label="Date picker inline"
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={12}>
+            <FormControlLabel component="legend">Gender</FormControlLabel>
+              <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                <FormControlLabel value="other" control={<Radio />} label="Other" />
+                <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+                <Link color="secondary" to={'/signin'}>
+                    Already have an account? Sign in
+                </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
---
+++ wwwroot/webpack.config.js
@@ -0,0 +1,71 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import Dotenv from "dotenv-webpack";
+
+
+module.exports = {
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      publicPath: "/",
+      filename: "[name].bundle.js",
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          node_vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            chunks: "all",
+            priority: 1,
+          },
+        },
+        chunks: "all",
+      },
+    },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+      extensions: [".js", ".jsx", ".json"],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "src"),
+      historyApiFallback: true,
+      compress: true,
+      port: 3000,
+    },
+    module: {
+      rules: [
+        {
+          // this is so that we can compile any React,
+          // ES6 and above into normal ES5 syntax
+          test: /\.(js|jsx)$/,
+          // we do not want anything from node_modules to be compiled
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader", // compiles Sass to CSS, using Node Sass by default
+          ],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif|mp3|svg|ico)$/,
+          loader: ["file-loader"],
+        },
+        {
+          test: /\.(ttf|eot|woff|svg)$/,
+          loader: ["url-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+        favicon: "./public/favicon.ico"
+      }),
+      new Dotenv(),
+    ],
+  };
