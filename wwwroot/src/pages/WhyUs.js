c64c6917d7bd2850df4128540b93df7624657ccf
KHong <albtrossfacefall@gmail.com>
2020-04-15 17:53:16 -0400

    add copy into whyUsData

--- wwwroot/src/pages/WhyUs.js
+++ wwwroot/src/pages/WhyUs.js
@@ -8,6 +8,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import bannerImg from '../../public/whyus-banner.jpg';
 import gettingImg from '../../public/whyus-gettingbetter.jpg';
 
+const whyUsData = {
+  topQuote: {
+    body: '"Education is the passport to the future, for tomorrow belongs to those who prepare for it today."',
+    source: '- Malcom X'
+  },
+  bodyCopy: {
+    boldParseInput: 'We believe _everyone_ should have access to an _uncompromised_ education online and offline.',
+    body: 'We are a team of teachers, students, engineers and designers that are passionate about continuing a full education to students everywhere now and for the future. Our hope is that this platform brings positive impact and connects humans everywhere.'
+  }
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {}
 }))
